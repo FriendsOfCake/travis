cf997aa2ab403872610b2a718ed4283955dc45e4
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2013-09-21 18:30:14 -0400

    Run phpcs against tests. Closes #4

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 if [ "$PHPCS" != '1' ]; then
     ./Console/cake test $PLUGIN_NAME All$PLUGIN_NAME --stderr --coverage-clover build/logs/clover.xml;
 else
-    phpcs -p --ignore='*/Test/*' --extensions=php --standard=CakePHP ./Plugin/$PLUGIN_NAME;
+    phpcs -p --extensions=php --standard=CakePHP ./Plugin/$PLUGIN_NAME;
 fi
 
