78bacb807362a3c7a7f9fac8cb935ccb2ce5e599
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2014-08-29 00:16:43 -0400

    Add docblock

--- example_plugin/Model/Post.php
+++ example_plugin/Model/Post.php
@@ -4,6 +4,11 @@ App::uses('AppModel', 'Model');
 
 class Post extends AppModel {
 
+/**
+ * Retrieves all records
+ *
+ * @return array
+ */
 	public function getAll() {
 		return $this->find('all', array(
 			'order' => array('Post.id' => 'ASC')
